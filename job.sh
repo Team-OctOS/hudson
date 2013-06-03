753f9fe1d6e92480df259f2bc282b71403f90f3a
nocoast <voteforpedro8@gmail.com>
2013-06-03 03:34:47 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@ echo $ANDROID_JAVA_HOME
 
 if [ ! -d hudson ]
 then
-  git clone git://github.com/CarbonDev/hudson.git
+  git clone git://github.com/CarbonDev/hudson.git -b jb2 --single-branch
 fi
 
 cd hudson
